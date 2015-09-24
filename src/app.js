b12813cb3cb2de622af62d25da333bd3d089803e
Anderson Aguiar <revsoft10@gmail.com>
2015-09-23 21:35:23 -0300

    Fix express static

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ routes.get('/', function(req, res) {
   res.sendFile('public/index.html', {root: __dirname });
 });
 
-routes.use(express.static('src'));
+routes.use(express.static('src/public'));
 
 module.exports = routes;
